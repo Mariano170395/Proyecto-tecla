5e44c8792903fd3ed9319b6abe6fb6e94502377e
Mariano Estrada <mariano-e-s@hotmail.com>
2021-12-01 13:19:14 -0600

    se agregaron ejercicios faltantes

---
+++ Proyecto/Ejercicio-12/app.js
@@ -0,0 +1,4 @@
+//Desarrollar un algoritmo (pseudocódigo) que permita leer tres valores de likes de la red social por foto y almacenarlos en las variables A, B y C respectivamente. 
+//NOTA: El algoritmo debe imprimir cuál es el mayor y cuál es el menor. Recuerda constatar que los tres valores introducidos por el teclado sean valores distintos. 
+
+//Presentar un mensaje de alerta en caso de que se detecte la introducción de valores iguales.  
