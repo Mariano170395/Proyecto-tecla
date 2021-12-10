5a5b595506dc339a36019d148e230c7470061bf8
Mariano Estrada <mariano-e-s@hotmail.com>
2021-12-10 14:14:31 -0600

    agregue ejercicios faltantes y 19

--- Proyecto/Ejercicio-18/ejercicio18.js
+++ Proyecto/Ejercicio-18/ejercicio18.js
@@ -1,26 +1,42 @@
 // Ejercicio 18
-let moment = require('moment');
+let moment = require("moment");
+let express = require("express");
+
+//Creo servidor
+
+const server = express ();
+
+server.listen(3000, ()=>{
+    console.log('Wenas tardes bienvenido a su servilleta')
+});
+
+server.get('/', (req, res)=>{
+    res.send('Bienvenido sea uste a su servilleta')
+})
 
 //Hora Local
-let muestraHoraLocal = moment(new Date())
-console.log(muestraHoraLocal)
+let muestraHoraLocal = moment(new Date());
+console.log(muestraHoraLocal);
 
 //Horario Utc
-let muestraUtc = moment(new Date()).utc()
-console.log(muestraUtc)
+let muestraUtc = moment(new Date()).utc();
+console.log(muestraUtc);
 
-//Cálculo de la diferencia horaria entre hora local y UTC. 
-let diferencia = moment(muestraUtc.diff(muestraHoraLocal))
-console.log(`La diferencia de horas entre la hora local y la utc es de ${diferencia.hours()} horas`)
+//Cálculo de la diferencia horaria entre hora local y UTC.
+let diferencia = moment(muestraUtc.diff(muestraHoraLocal));
+console.log(
+  `La diferencia de horas entre la hora local y la utc es de ${diferencia.hours()} horas`
+);
 
-//La mayor de las dos fechas -luego de realizar la comparación entre ambas- 
-if(muestraHoraLocal>muestraUtc){
-    console.log('La hora local es mayor')
+//La mayor de las dos fechas -luego de realizar la comparación entre ambas-
+if (muestraHoraLocal > muestraUtc) {
+  console.log("La hora local es mayor");
 } else {
-    console.log('La hora Utc es mayor')
+  console.log("La hora Utc es mayor");
 }
 
 // Instalar la librería cool-images, y crear un script para traer una imagen de manera aleatoria.
-const coolImages = require('cool-images');
-let imagen = coolImages.one()
-console.log(imagen)
+const coolImages = require("cool-images");
+let imagen = coolImages.one();
+console.log(imagen);
+
---
+++ Proyecto/Ejercicio-19/app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const server = express();
+
+//Inicializar servidor
+
+server.listen(3000, () => {
+  console.log("Servidor inicializado");
+});
+
+//Endpoint home
+server.get("/", (req, res) => {
+  res.json("Holi esto es home");
+});
+
+//Middleware para validar datos
+let auth = false;
+
+let validar = (req, res, next) =>{
+    if(auth === true){
+        next()
+    } else {
+        res.json('Que haces aqui?')
+    }
+}
+
+//Inicializo mi array
+let usuarios = [
+  {
+    id: 1,
+    nombre: "Danilo",
+    email: "danilo@tecla.academy",
+  },
+  {
+    id: 2,
+    nombre: "Hugo",
+    email: "Hugo@tecla.academy",
+  },
+  {
+    id: 3,
+    nombre: "Juan",
+    email: "juan@tecla.academy",
+  },
+];
+
+//Endpoint para mostrar usuarios
+server.get('/usuarios', validar, (req, res)=>{
+    res.json(usuarios)
+})
+
+//filtro usuarios por id
+let filtraUsers = (id) =>{
+    for(let usuario of usuarios){
+        if(id === usuario.id){
+            return(usuario)
+        }
+    }
+}
+
+//filtro usuarios por nombre
+let filtraUsersNombre = (nombre) =>{
+    for(let usuario of usuarios){
+        if(nombre === usuario.nombre){
+            return(usuario)
+        }
+    }
+}
+
+
+//endpoint id
+server.get('/id', validar, (req, res)=>{
+    res.json(filtraUsers(2))
+})
+
+//endpoint nombres
+server.get('/nombre', validar, (req, res)=>{
+    res.json(filtraUsersNombre('Hugo'))
+})
